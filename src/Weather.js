bf44f0f757400b9d91a74091441a10f31e5a2d79
Alina Smahliuk <88776329+SmahliukAlina@users.noreply.github.com>
2022-06-01 15:55:38 +0300

    Current weather feature html/css done

--- src/Weather.js
+++ src/Weather.js
@@ -33,7 +33,46 @@ export default function Weather() {
           </div>
 
           <div className="row">
-            <div className="col col-left"></div>
+            <div className="col col-left">
+              <div className="cur-city-title" id="city">
+                Helsinki
+              </div>
+              <div className="row">
+                <div className="col cur-date">
+                  Last upd: <span id="cur-date">Wed, Jun 01, 15:48</span>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col" id="cur-weather">
+                  <div className="cur-temperature">
+                    <span className="temp-value" id="cur-temp-value">
+                      19
+                    </span>
+                    <span className="unit cur-unit" id="cur-unit">
+                      °C
+                    </span>
+                  </div>
+                  <div className="cur-feels-like-title">
+                    Feels like <span id="cur-feels-like-temp">20</span>
+                    <span className="unit cur-unit">°C</span>
+                  </div>
+                  <div className="weather-emoji" id="cur-weather-emoji">
+                    ☀️
+                  </div>
+                  <div className="row">
+                    <div className="col" id="weather-decription">
+                      Sunny
+                    </div>
+                    <div className="col">
+                      <span className="wind-title">
+                        Wind: <span id="wind">2</span> m/h
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <div className="col col-right"></div>
           </div>
         </div>
