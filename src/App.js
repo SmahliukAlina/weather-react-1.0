d1a01719855e0d7e364261f572ca1fa0975a9010
Alina Smahliuk <88776329+SmahliukAlina@users.noreply.github.com>
2022-06-01 14:53:27 +0300

    Footer with links on GitHub and Portfolio site addded

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>try number 34</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://classy-semolina-bd4086.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Alina`s Portfolio Site"
+          >
+            Alina Smahliuk
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/SmahliukAlina/weather-react-1.0"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Project on GitHub"
+          >
+            open-sourced
+          </a>
+        </footer>
       </div>
     </div>
   );
