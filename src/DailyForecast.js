65a2551c2cff9f9fa5c2c4c35df2f2acb0a5ac55
Alina Smahliuk <88776329+SmahliukAlina@users.noreply.github.com>
2022-06-03 00:21:24 +0300

    DayForecast separated component

---
+++ src/DailyForecast.js
@@ -0,0 +1,48 @@
+import React from "react";
+import FormatWeekDay from "./FormatWeekDay";
+import FormatMonth from "./FormatMonth";
+import WeatherIcon from "./WeatherIcon";
+
+export default function DailyForecast(props) {
+  function maxTemp() {
+    let maxTemp = Math.round(props.data.temp.max);
+    return `${maxTemp}`;
+  }
+
+  function minTemp() {
+    let minTemp = Math.round(props.data.temp.min);
+    return `${minTemp}`;
+  }
+
+  let date = new Date(props.data.dt * 1000);
+  let emojiCode = props.data.weather[0].icon;
+
+  return (
+    <div className="DailyForecast">
+      <div className="forecast-title">Forecast</div>
+      <div className="row">
+        <div className="col-4 weekday" id="weekday">
+          <div className="day" id="day">
+            <FormatWeekDay form="long" dayIndex={date.getDay()} />
+          </div>
+          <div className="date" id="date">
+            <FormatMonth monthIndex={date.getMonth()} /> {date.getDate()}
+          </div>
+        </div>
+        <div className="col-5 temperature">
+          <span className="temp-value max-temp-value" id="max-temperature">
+            {maxTemp()}
+          </span>
+          <span className="unit">°C</span>/{" "}
+          <span className="temp-value min-temp-value" id="min-temperature">
+            {minTemp()}
+          </span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-3 weather-emoji" id="weather-emoji">
+          <WeatherIcon iconCode={emojiCode} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Forecast.js
+++ src/Forecast.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Forecast.css";
-import FormatWeekDay from "./FormatWeekDay";
-import FormatMonth from "./FormatMonth";
-import WeatherIcon from "./WeatherIcon";
+
+import DailyForecast from "./DailyForecast";
 
 export default function Forecast(props) {
   let apiKey = "f7ab8c50642226d2981457d7445b4fa2";
@@ -12,44 +11,16 @@ export default function Forecast(props) {
   let [forecastData, setForecastData] = useState({ ready: false });
 
   function handleResponse(response) {
-    console.log(response);
     setForecastData({
-      timestamp: response.data.daily[2].dt * 1000,
-      minTemp: Math.round(response.data.daily[2].temp.min),
-      maxTemp: Math.round(response.data.daily[2].temp.max),
-      emojiCode: response.data.daily[2].weather[0].icon,
+      weatherDaily: response.data.daily,
       ready: true,
     });
   }
 
   if (forecastData.ready) {
-    let now = new Date(forecastData.timestamp);
     return (
       <div className="Forecast">
-        <div className="forecast-title">Forecast</div>
-        <div className="row">
-          <div className="col-4 weekday" id="weekday">
-            <div className="day" id="day">
-              <FormatWeekDay form="long" dayIndex={now.getDay()} />
-            </div>
-            <div className="date" id="date">
-              <FormatMonth monthIndex={now.getMonth()} /> {now.getDate()}
-            </div>
-          </div>
-          <div className="col-5 temperature">
-            <span className="temp-value max-temp-value" id="max-temperature">
-              {forecastData.maxTemp}
-            </span>
-            <span className="unit">°C</span>/
-            <span className="temp-value min-temp-value" id="min-temperature">
-              {forecastData.minTemp}
-            </span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-3 weather-emoji" id="weather-emoji">
-            <WeatherIcon iconCode={forecastData.emojiCode} />
-          </div>
-        </div>
+        <DailyForecast data={forecastData.weatherDaily[1]} />
       </div>
     );
   } else {
